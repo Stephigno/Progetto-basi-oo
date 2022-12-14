3683680e268317dd1894e0f2252115dc6302859a
hedge16 <vincenzoriccio166@gmail.com>
2022-12-14 01:42:32 +0100

    Inizializzazione della GUI

--- Galleria_geolocalizzata/src/main/java/GUI/Login.java
+++ Galleria_geolocalizzata/src/main/java/GUI/Login.java
@@ -1,4 +1,17 @@
 package GUI;
+import javax.swing.*;
 
 public class Login {
+
+    private static JFrame frame1;
+    private  JButton loginButton;
+    private JButton resetButton;
+    private JTextField userIDField;
+    private JPasswordField userPassField;
+    private JLabel userIDLabel;
+    private JLabel userPassLabel;
+    private JPanel rootPanel;
+    private JLabel loginLabel;
+
+
 }
